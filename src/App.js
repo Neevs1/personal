29ef5699e8bf24b93488dee07e701fba21fddb5c
Neevs1 <neevan.redkar@gmail.com>
2024-10-21 23:00:45 +0530

    Added new using react

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Neevan Redkar</h1>
+      </header>
+      <div className="App-MainBody">
+      <h2>Welcome to my Website!</h2>
+      <div className="App-Content">
+        <div class="App-Card">
+          <h3>About Me</h3>
+        </div>
+        <div class="App-Card">
+          <h3>Contact Me</h3>
+        </div>
+        <div class="App-Card">
+          <h3>Projects</h3>
+        </div>
+
+       </div> 
+      </div>
+      
+      <footer className="App-footer">
+        <marquee>Created by Neevan Redkar</marquee>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
